5360246408a57387ad5c009f2313c4d00b9c566c
FernandoPatrick <78447989+FernandoPatrick@users.noreply.github.com>
2021-07-11 20:26:05 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+$("#enviar").click(
+    enviarmensagem()
+)
+
+    function enviarmensagem(){                                
+        $.ajax({
+            type:'POST',
+            url:'http://192.168.10.14:8091/mail/send',
+            contentType: "application/json",
+            crossDomain: true,
+            headers:{
+                Authorization : "Basic "+btoa("ads:adsunifasipe2021")
+            },
+            data: '{"nome":"Willian","email":"willian.hubner",'+
+            '"site":"ola","corpo":"adhaushduashdusahd"}',
+            success: function(data){
+                console.log(data);                                        
+            },
+            error: function(data){
+                console.log(data);            
+            }
+        })
+    }
